b8dfb539824fc26feac4b6495b1bd63711d0ba39
ZhaoZhang <zhaozhang@thoughtworks.com>
2020-07-30 12:12:16 +0800

    rename

---
+++ 02-config-datasource/src/main/java/com/itutry/configdatasource/ConfigDatasourceApplication.java
@@ -0,0 +1,13 @@
+package com.itutry.configdatasource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigDatasourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigDatasourceApplication.class, args);
+	}
+
+}
---
+++ 02-config-datasource/src/test/java/com/itutry/configdatasource/ConfigDatasourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itutry.configdatasource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigDatasourceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
